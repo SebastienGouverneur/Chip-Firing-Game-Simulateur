d3bf11cf5e2edbea0438152ee93f61cfe6ba711e
Abde <abde3.khe@gmail.com>
2015-04-23 13:31:16 +0200

    Animation of algorithms on graph. TO FINISH

--- src/core/ModeSequentialBlock.java
+++ src/core/ModeSequentialBlock.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
@@ -14,17 +16,13 @@ public class ModeSequentialBlock implements IMode {
     public void execute(Graph graph) {
         LinkedHashMap<Integer, LinkedList<String>> patternUpdate = new LinkedHashMap<>();
 
-        {
+        int count = 0;
+        for (Node node : graph) {
             LinkedList<String> patternIteration = new LinkedList<>();
-            patternIteration.add("A");
-            patternIteration.add("B");
-            patternUpdate.put(0, patternIteration);
+            patternIteration.add(node.getId());
+            patternUpdate.put(count++, patternIteration);
         }
-        {
-            LinkedList<String> patternIteration = new LinkedList<>();
-            patternIteration.add("C");
-            patternUpdate.put(1, patternIteration);
-        }
+
 
         /* start */
         for (Map.Entry<Integer, LinkedList<String>> stepIter : patternUpdate.entrySet()) {
@@ -41,12 +39,33 @@ public class ModeSequentialBlock implements IMode {
                     for (Edge edgeOut : graph.getNode(entry.getKey()).getEachLeavingEdge()) {
                         edgeOut.getNode1().setAttribute("chips", (int) edgeOut.getNode1().getAttribute("chips") + 1);
                         edgeOut.getNode0().setAttribute("chips", (int) edgeOut.getNode0().getAttribute("chips") - 1);
+                        edgeOut.setAttribute("ui.class", "actif");
                     }
                 }
+
+//                for (String parallel : parallelPattern) {
+//                    for (Edge edgeOut : graph.getNode(parallel).getEachLeavingEdge()) {
+//                        edgeOut.setAttribute("ui.class", "actif");
+//                    }
+//                }
+
+                graph.getNode(entry.getKey()).setAttribute("ui.label", graph.getNode(entry.getKey()).getAttribute("chips"));
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ModeSequentialBlock.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                for (String parallel : parallelPattern) {
+                    for (Edge edgeOut : graph.getNode(parallel).getEachLeavingEdge()) {
+                        edgeOut.setAttribute("ui.class", "");
+                    }
+                }
+
             }
-
         }
-
+        
         for (Node node : graph) {
             System.out.println(node.getId() + " : " + node.getAttribute("chips"));
         }
